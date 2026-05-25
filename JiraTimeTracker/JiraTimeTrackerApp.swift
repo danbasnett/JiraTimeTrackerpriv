12e2ed07a46b60a7b713a2adb09d184bbfb44e8f
danbasnett <danielbasnett@gmail.com>
2026-05-25 23:01:28 +0100

    Initial Commit

---
+++ JiraTimeTracker/JiraTimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JiraTimeTrackerApp.swift
+//  JiraTimeTracker
+//
+//  Created by daniel basnett on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct JiraTimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
